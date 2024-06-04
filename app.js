68d94c3b792db150db82a7a4efdd84e50705f534
hassan memon <bhaihassan036@gmail.com>
2024-06-04 14:30:40 +0500

    file name set

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask(){
    if(inputBox.value === ''){
       alert("You must write something!");
